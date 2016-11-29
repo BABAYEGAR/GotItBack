4552b8e53c6724c51b565b4a8c1ac2246e05236f
Haruna Salim <salxsaa@gmail.com>
2016-11-30 00:11:12 +0100

    Add project files.

---
+++ GotItBack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GotItBack.MvcApplication" Language="C#" %>
---
+++ GotItBack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GotItBack.Startup))]
+namespace GotItBack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
