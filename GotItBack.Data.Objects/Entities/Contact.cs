e9436f636bb0da4a0d0db29ed3dec42ae0117830
Haruna Salim <salxsaa@gmail.com>
2016-11-30 00:56:43 +0100

    Added Properties to teh contact object

--- GotItBack.Data.Objects/Entities/Contact.cs
+++ GotItBack.Data.Objects/Entities/Contact.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace GotItBack.Data.Objects.Entities
 {
     public class Contact
     {
+        [Key]
         public long ContactId { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }
