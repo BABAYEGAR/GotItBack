16846f871f519a76d6322a7661cc817fff29ad3d
Haruna Salim <salxsaa@gmail.com>
2016-11-30 01:53:26 +0100

    Added the sub categories Controller... Added the create,edit,view and delete views for sub categories

---
+++ GotItBack/Controllers/GotItBackControllers/SubCategoriesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GotItBack.Data.Context.DataContext;
+using GotItBack.Data.Objects.Entities;
+
+namespace GotItBack.Controllers.GotItBackControllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private SubCategoryDataContext db = new SubCategoryDataContext();
+
+        // GET: SubCategories
+        public ActionResult Index()
+        {
+            var subCategories = db.SubCategories.Include(s => s.Category);
+            return View(subCategories.ToList());
+        }
+
+        // GET: SubCategories/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategory subCategory = db.SubCategories.Find(id);
+            if (subCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        // POST: SubCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SubCategoryId,Name,CategoryId")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SubCategories.Add(subCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategory subCategory = db.SubCategories.Find(id);
+            if (subCategory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SubCategoryId,Name,CategoryId")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", subCategory.CategoryId);
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SubCategory subCategory = db.SubCategories.Find(id);
+            if (subCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            SubCategory subCategory = db.SubCategories.Find(id);
+            db.SubCategories.Remove(subCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
