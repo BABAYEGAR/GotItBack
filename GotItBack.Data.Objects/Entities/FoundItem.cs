f9fd019c0c3009ebf44ee0b42f5fdfd28fd1e01c
Haruna Salim <salxsaa@gmail.com>
2016-11-30 01:01:12 +0100

    Added The sub category object with its properties

---
+++ GotItBack.Data.Objects/Entities/FoundItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotItBack.Data.Objects.Entities
+{
+    public class FoundItem
+    {
+        public long FoundItemId { get; set; }
+        public DateTime DateItemFound { get; set; }
+    }
+}
